639163a181b2d9cc23cc0c707f89ec98998f9d57
CharlesOsang017 <charlesosango02@gmail.com>
2026-03-28 18:02:04 +0300

    Fixed deployment bug

--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {} from "../middlewares/auth.middleware.js";
+import { createTask, deleteTask, updateTask } from "../controllers/task.controller.js";
 
 const router = express.Router()
 
